0766ba422db5ab31b50744282fce515c42c7b28d
gjd <george.j.danis@gmail.com>
2015-07-12 16:18:21 -0400

    for merge

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pywapi
 
-if __name__ == "__main__":
+weather = None
+
+def main():
+    global weather
     questions = ["zip code", "days", "nights",
                      "exercise (y/n)", "formal wear needed (y/n)"]
 
     trip = { q: input(q + "? ").lower() for q in questions }
-    
-    
+    weather = pywapi.get_weather_from_weather_com(trip["zip code"], units="")
+
+if __name__ == "__main__":
+    main()
 
