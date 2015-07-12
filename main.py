c2341ac793fdeaeefc8d6a0f6b5484969adec26b
gjd <george.j.danis@gmail.com>
2015-07-12 15:46:30 -0400

    add queries; create query dictionary

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pywapi
+import sys
+
+class Item:
+    pass
+
+class Clothing(Item):
+    pass
+
+
+if __name__ == "__main__":
+    questions = ["zip code", "days", "nights",
+                     "exercise (y/n)", "formal wear needed (y/n)"]
+
+    trip = { q: input(q + "? ").lower() for q in questions }
+    
+    
+
