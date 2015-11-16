c8cdfa2139faa181f3ca05f7cabe230f59ba775a
Ben E. Whitney <ben.e.whitney@post.harvard.edu>
2015-11-15 22:06:32 -0500

    Add argument parsing and a config file.

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
 import pywapi
 
-import xml.etree.ElementTree as ETR
+import argparse
+import json
 import math
+import os
 import sys
+import xml.etree.ElementTree as ETR
 
 #TODO: import some type of logging framework.
-QUESTIONS = ('ZIP code', 'days', 'exercise (y/n)', 'formal wear needed (y/n)')
 WATER_FREEZING_POINT = 32
+DEFAULT_PREFERENCES = {
+    'rain_threshold': 0.1,
+    'snow_threshold': 0.1,
+}
+
+parser = argparse.ArgumentParser(description=
+                                 'Produce a packing list for a trip.')
+parser.add_argument('zip_code', help='ZIP code of destination',
+                    metavar='ZIP-code')
+parser.add_argument('--formal-wear', help='whether formal wear will be needed',
+                    action='store_true')
+parser.add_argument('--exercise', help='whether exercise clothes will be '
+                    'needed', action='store_true')
+parser.add_argument('--config-file', help='custom location of configuration '
+                    'file', default=os.path.join(os.environ['HOME'],
+                                                 '.pack-me-up'))
+parser.add_argument('days', help='duration of trip in days')
+args = parser.parse_args()
+
+if os.path.isfile(args.config_file):
+    with open(args.config_file, 'r') as f:
+        preferences = json.load(f)
+    #TODO: make this more user-friendly.
+    for key in DEFAULT_PREFERENCES:
+        assert key in preferences
+else:
+    preferences = DEFAULT_PREFERENCES
+    print('writing a default config file to {fil} ...'.format(
+        fil=args.config_file))
+    with open(args.config_file, 'w') as f:
+        json.dump(preferences, f)
 
 class Forecast:
-    def __init__(self, weather, days, preferences):
+    def __init__(self, weather, days):
         self.minimum_temperature = min(float(daily['low']) for daily in
                                        weather['forecasts'][1:days+1])
         self.minimum_temperature = max(float(daily['high']) for daily in
@@ -27,8 +60,8 @@ class Forecast:
                 prob_no_rain *= prob_no_precip
             if float(daily['low']) <= WATER_FREEZING_POINT:
                 prob_no_snow *= prob_no_precip
-        self.rain = 1-prob_no_rain >= preferences.rain_threshold
-        self.snow = 1-prob_no_snow >= preferences.snow_threshold
+        self.rain = 1-prob_no_rain >= preferences['rain_threshold']
+        self.snow = 1-prob_no_snow >= preferences['snow_threshold']
 
 class Item:
     def __init__(self, **kwargs):
@@ -96,10 +129,10 @@ class ClothingItem(DurationDependentItem, WeatherDependentItem):
                 super(WeatherDependentItem).evaluate(**kwargs))
 
 def main():
-    trip = {question: input('{que}: ').format(que=question).lower()
-            for question in QUESTIONS}
-    weather = Forecast(pywapi.get_weather_from_weather_com(trip['ZIP code'],
-                                                           units='imperial'))
+    weather = Forecast(
+        pywapi.get_weather_from_weather_com(args.zip_code, units='imperial'),
+        args.days,
+    )
 
 if __name__ == '__main__':
     main()
